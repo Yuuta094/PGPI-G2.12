bc3978481575affe3c16dfc79d20575094f2dea4
mararopae <mararopae@us.es>
2023-11-12 16:17:33 +0100

    feat: Listado de obras de arte en escaparate

--- artUS/artus/core/views.py
+++ artUS/artus/core/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
+from product.models import Obra
+
 # Create your views here.
 def index(request):
-    return render(request, 'core/index.html')
+    obras = Obra.objects.all()[0:3]
+
+    return render(request, 'core/index.html', {
+        'obras': obras
+    })
 
 def about(request):
-    return render(request, 'core/about.html')
+    return render(request, 'core/about.html', {
+    })
