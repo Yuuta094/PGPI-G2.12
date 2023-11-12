2b5fc39273647f829734745e16ac67d9bcd6e3ac
laurolmer <xxlaurarmxx@gmail.com>
2023-11-12 13:40:04 +0100

    Corrección del idioma de programación.

--- artUS/artus/core/models.py
+++ artUS/artus/core/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
-class Clientes(models.Model):
+class Client(models.Model):
     name = models.CharField(max_length=100)
     surname = models.CharField(max_length=100)
     username = models.CharField(max_length=100)
     password = models. password = models.CharField(max_length=50)
     email = models.EmailField(max_length=100)
 
-class Admin(models.Model):
+class Administrator(models.Model):
     name = models.CharField(max_length=100)
     surname = models.CharField(max_length=100)
     username = models.CharField(max_length=100)
--- artUS/artus/product/admin.py
+++ artUS/artus/product/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
-from .models import Obra
+from .models import Artwork
 
-admin.site.register(Obra)
+admin.site.register(Artwork)
