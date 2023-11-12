48f9b95f95f110e7e6c516e2bd2299a44d9b0a8e
mararopae <mararopae@us.es>
2023-11-12 11:58:42 +0100

    feat: Definit estructura del fronted de la pagina y hacer primer modelo de Obras

---
+++ artUS/artus/core/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'core/index.html')
+
+def about(request):
+    return render(request, 'core/about.html')
