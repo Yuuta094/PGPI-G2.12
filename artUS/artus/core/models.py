db008e041406f854985e0f3186197b1565053832
claugp06 <99924348+claugp06@users.noreply.github.com>
2023-11-12 15:15:12 +0100

    Revert "Creación del modelo Users y Admin con su valores"
    This reverts commit 77bbd0608bd280ff897c3926fea63d49ed181bfe.

--- artUS/artus/core/models.py
+++ artUS/artus/core/models.py
@@ -1,22 +1,3 @@
 from django.db import models
 
-class Clientes(models.Model):
-    name = models.CharField(max_length=100)
-    surname = models.CharField(max_length=100)
-    username = models.CharField(max_length=100)
-    password = models. password = models.CharField(max_length=50)
-    email = models.EmailField(max_length=100)
-
-class Admin(models.Model):
-    name = models.CharField(max_length=100)
-    surname = models.CharField(max_length=100)
-    username = models.CharField(max_length=100)
-    password = models. password = models.CharField(max_length=50)
-    email = models.EmailField(max_length=100)
-
-    class Meta:
-        ordering = ['name']
-
-    def __str__(self):
-        return self.name
-
+# Create your models here.
