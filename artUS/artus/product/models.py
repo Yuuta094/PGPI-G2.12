46eb31853cfa70514932e4491efb61c7c3f8c6cc
claugp06 <99924348+claugp06@users.noreply.github.com>
2023-11-12 19:33:02 +0100

    Arreglo de errores en el detalle de producto Se han arreglado diversos problemas así como se ha añadido una nueva caracteristica

--- artUS/artus/product/models.py
+++ artUS/artus/product/models.py
@@ -17,6 +17,7 @@ class Obra(models.Model):
     opinions = models.JSONField(null=True, blank=True)
     manufacturer = models.CharField(max_length=100)
     image = models.ImageField(upload_to='product_images', null=True, blank=True)
+    
 
     class Meta:
         ordering = ['name']
--- artUS/artus/product/views.py
+++ artUS/artus/product/views.py
@@ -3,4 +3,8 @@ from .models import Obra
 
 def detail(request,pk):
     obra = get_object_or_404(Obra, pk=pk)
-    return render(request,'product/detail.html',{'obra':obra})
+    related_obras = Obra.objects.filter(category=obra.category).exclude(pk=pk)
+    return render(request,'product/detail.html', {
+        'obra':obra,
+        'related_obras':related_obras
+    })
