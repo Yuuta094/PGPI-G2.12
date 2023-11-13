105fd415936a3568078226424b9abd4c127f2515
enrcabmun <enrcabmun@alum.us.es>
2023-11-13 06:34:19 +0100

    add(product): search and filter functionality

--- artUS/artus/product/urls.py
+++ artUS/artus/product/urls.py
@@ -3,4 +3,6 @@ from . import views
 app_name = 'product'
 urlpatterns = [
     path('<int:pk>/',views.detail,name='detail'),
+    path('search/',views.search_page,name='search_page'),
+    path('search/result/',views.search,name='search'),
 ]
--- artUS/artus/product/views.py
+++ artUS/artus/product/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,get_object_or_404
-from .models import Obra
+from .models import Category, Obra
 
 def detail(request,pk):
     obra = get_object_or_404(Obra, pk=pk)
@@ -7,4 +7,32 @@ def detail(request,pk):
     return render(request,'product/detail.html', {
         'obra':obra,
         'related_obras':related_obras
+    })
+    
+def search_page(request):
+    categories = [choice[1] for choice in Category.choices]
+    return render(request,'product/search.html', {
+        'categories':categories
+    })
+    
+def search(request):
+    categories = [choice[1] for choice in Category.choices]
+    q = request.GET.get('q')
+    category = request.GET.get('category')
+    author = request.GET.get('author')
+    min_price = request.GET.get('min_price')
+    max_price = request.GET.get('max_price')
+    obras = Obra.objects.filter(name__icontains=q, category__icontains=category, author__icontains=author)
+    if min_price:
+        obras = obras.filter(price__gte=min_price)
+    if max_price:
+        obras = obras.filter(price__lte=max_price)
+    return render(request,'product/search.html', {
+        'obras':obras,
+        'q':q,
+        'categories':categories,
+        'category':category,
+        'author':author,
+        'min_price':min_price,
+        'max_price':max_price
     })
