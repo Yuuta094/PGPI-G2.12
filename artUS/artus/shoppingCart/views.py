89bca2a427121b3fc4feee6df269fa4589611890
claugp06 <99924348+claugp06@users.noreply.github.com>
2023-11-24 07:52:38 +0100

    Fix: carrito de la compra

--- artUS/artus/shoppingCart/views.py
+++ artUS/artus/shoppingCart/views.py
@@ -6,27 +6,42 @@ from django.urls import reverse
 
 
 from decimal import Decimal
+from django.contrib import messages
 
 def add_shoppingcart_from_product_detail(request, artwork_id):
-   shopping_cart = request.session.get('shopping_cart', {})
-   artwork = Artwork.objects.get(id=artwork_id)
-   if str(artwork_id) in shopping_cart:
-       shopping_cart[str(artwork_id)]['quantity'] += 1
-       shopping_cart[str(artwork_id)]['accum_value'] = float(shopping_cart[str(artwork_id)]['accum_value']) + float(artwork.price)
-   else:
-       shopping_cart[str(artwork_id)] = {'accum_value': float(artwork.price), 'quantity': 1}
-   request.session['shopping_cart'] = shopping_cart
-   return redirect('product:detail', artwork_id=artwork_id)
+    shopping_cart = request.session.get('shopping_cart', {})
+    artwork = Artwork.objects.get(id=artwork_id)
+    quantity = int(request.POST.get('quantity', 1))  # get the quantity from the form
+
+   
+    if quantity > artwork.quantity:
+        messages.error(request, 'La cantidad solicitada excede la cantidad disponible de esta obra.')
+        return redirect('product:detail', artwork_id=artwork_id)
+
+    if str(artwork_id) in shopping_cart:
+        shopping_cart[str(artwork_id)]['quantity'] += quantity
+        shopping_cart[str(artwork_id)]['accum_value'] = float(shopping_cart[str(artwork_id)]['accum_value']) + float(artwork.price) * quantity
+    else:
+        shopping_cart[str(artwork_id)] = {'accum_value': float(artwork.price) * quantity, 'quantity': quantity}
+    request.session['shopping_cart'] = shopping_cart
+    request.session.modified = True
+    return redirect('product:detail', artwork_id=artwork_id)
 
 
 def add_shoppingcart_from_cart(request, artwork_id):
     shopping_cart = request.session.get('shopping_cart', {})
     artwork = Artwork.objects.get(id=artwork_id)
+
+    # Check if requested quantity exceeds available quantity
+    if str(artwork_id) in shopping_cart and shopping_cart[str(artwork_id)]['quantity'] + 1 > artwork.quantity:
+        messages.error(request, 'La cantidad solicitada excede la cantidad disponible de esta obra.')
+        return redirect('shoppingCart:carrito_detail')
+
     if str(artwork_id) in shopping_cart:
-       shopping_cart[str(artwork_id)]['quantity'] += 1
-       shopping_cart[str(artwork_id)]['accum_value'] = float(shopping_cart[str(artwork_id)]['accum_value']) + float(artwork.price)
+        shopping_cart[str(artwork_id)]['quantity'] += 1
+        shopping_cart[str(artwork_id)]['accum_value'] = float(shopping_cart[str(artwork_id)]['accum_value']) + float(artwork.price)
     else:
-       shopping_cart[str(artwork_id)] = {'accum_value': float(artwork.price), 'quantity': 1}
+        shopping_cart[str(artwork_id)] = {'accum_value': float(artwork.price), 'quantity': 1}
     request.session['shopping_cart'] = shopping_cart
     request.session.modified = True
     return redirect('shoppingCart:carrito_detail')
