c760a5848b54b2e44727b7dd00262008df425f4c
Yuuta094 <YvanSan094@gmail.com>
2023-12-02 21:26:52 +0100

    Secciones Destacadas

--- artUS/artus/core/forms.py
+++ artUS/artus/core/forms.py
@@ -1,7 +1,8 @@
 from django import forms 
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import PasswordChangeFormfrom django.contrib.auth import authenticate
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth import authenticate
 from django.contrib.auth import get_user_model
 
 class LoginForm(forms.Form):
