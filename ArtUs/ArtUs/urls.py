f5a1bcccae9e6ba64716bcb5d6c7dc080cc610d1
claugp06 <99924348+claugp06@users.noreply.github.com>
2023-12-07 09:47:12 +0100

    Fix local

--- ArtUs/ArtUs/urls.py
+++ ArtUs/ArtUs/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for ArtUs project.
+URL configuration for artus project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
