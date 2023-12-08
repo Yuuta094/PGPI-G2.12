341f2c45a1a8aec2622ead3fad1ed8cb47d4f5b5
claugp06 <99924348+claugp06@users.noreply.github.com>
2023-12-08 10:12:58 +0100

    Update wsgi.py para docker

--- ArtUs/ArtUs/wsgi.py
+++ ArtUs/ArtUs/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'artus.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ArtUs.settings')
 
 application = get_wsgi_application()
